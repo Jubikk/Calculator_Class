775bf08d47fb63d12d0728e1632694fb34b54fa8
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-05-24 21:39:29 +0800

    Pseudocode

---
+++ Calculator_Class.py
@@ -0,0 +1,11 @@
+#Joebeck Andrew F. Gusi | BSCPE 1-5 | Assignment No.7 |
+
+
+#Pseudocode
+
+#Create a Calculator Class
+    #Inside the class, define the operation such as addition, subtraction, multiplication, and division.
+    #Include a ZeroDivisonError
+
+#End
+
