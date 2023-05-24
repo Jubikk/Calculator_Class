219ae44b990f067d54cb5a3dd05321c3309001b0
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-05-24 22:00:51 +0800

    the calculator class is added

--- Calculator_Class.py
+++ Calculator_Class.py
@@ -1,11 +1,16 @@
 #Joebeck Andrew F. Gusi | BSCPE 1-5 | Assignment No.7 |
 
-
-#Pseudocode
-
-#Create a Calculator Class
-    #Inside the class, define the operation such as addition, subtraction, multiplication, and division.
-    #Include a ZeroDivisonError
+class MyCalculator:
+    def add(self, integer1, integer2):
+        return integer1 + integer2
+    def subtract(self, integer1, integer2):
+        return integer1 - integer2
+    def multiply(self, integer1, integer2):
+        return integer1 * integer2
+    def divide(self, integer1, integer2):
+        if integer2 != 0:
+            return integer1 / integer2
+        else:
+            return "Cannot Divide by zero!"
 
 #End
-
