c54e78610d3f308d5f2da65b6cd10ab79abeb34d
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-05-25 19:18:29 +0800

    1st part of the pseudocode of the UI

---
+++ UserInterface_Class.py
@@ -0,0 +1,12 @@
+#Joebeck Andrew F. Gusi | BSCPE 1-5 | Assignment No.7 |
+
+#Pseudocode
+
+#Import tkinter, from tkinter import messagebox and simpledialog, and import time
+#Create a UserInterface Class
+    #def init
+    #def Set Calculator
+    #def message
+    #def menu
+    #def get integer and operation
+    
