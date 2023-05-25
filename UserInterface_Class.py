dcf74c78d98ebd334adca731bfa3f142fbebd814
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-05-25 21:18:47 +0800

    importing and adding the UI class

--- UserInterface_Class.py
+++ UserInterface_Class.py
@@ -1,9 +1,18 @@
 #Joebeck Andrew F. Gusi | BSCPE 1-5 | Assignment No.7 |
 
-#Pseudocode
-
 #Import tkinter, from tkinter import messagebox and simpledialog, and import time
+import tkinter as tk
+from tkinter import messagebox, simpledialog
+from Calculator_Class import MyCalculator
+import time
+
 #Create a UserInterface Class
+class UserInterface:
+    def __init__(self, root):
+        self.calculator = None
+        self.root = root
+        self.root.withdraw()
+        self.root.protocol("WM_DELETE_WINDOW", self.close_window)
     #def init
     #def Set Calculator
     #def message
